4e2ffeffb2e078c3d1506aca31f220f4735f0172
Efthakhar <efthakharbin@gmail.com>
2023-05-23 17:02:30 +0600

    Redesign mini Admin

--- dashboard-core.js
+++ dashboard-core.js
@@ -1,29 +1,30 @@
 
+
+
 // sidebar links sublinks toggle functionality
-let links = document.querySelectorAll('.has_sublink');
+let links = document.querySelectorAll('.admin-sidebar__link_item');
 
 for(i=0;i<links.length;i++){
-    links[i].addEventListener('click',(e)=>{
-        e.target.parentNode.querySelector('.sidebar__link_sublinks').classList.toggle('show_sublinks')
 
+    links[i].addEventListener('click',(e)=>{
+        
+        e.target.closest('.admin-sidebar__link_item')
+        .querySelector('.sidebar__link_sublinks')
+        .classList.toggle('show_sublinks')
     })
 }
+
 
 // sidebar hide or show toggle function
 let toggle_sidebar_icon = document.querySelector('.menu-icon');
 let admin_sidebar = document.querySelector('.admin-sidebar');
-let mobile_sidebar_close_btn = document.querySelector('.close-sidebar svg');
+let mobile_sidebar_close_btn = document.querySelector('.hide-small-device-sidebar svg');
 
 toggle_sidebar_icon.addEventListener('click',(e)=>{
-   admin_sidebar.classList.toggle('admin-sidebar-hide');
+   admin_sidebar.classList.toggle('admin-sidebar-hidden');
 })
+
 mobile_sidebar_close_btn.addEventListener('click',(e)=>{
-   admin_sidebar.classList.toggle('admin-sidebar-hide');
+   admin_sidebar.classList.toggle('admin-sidebar-hidden');
 })
 
-
-// setTimeout(()=>{
-//     //let rw =  'Qd'+Math.random(12,90);
-//     //window.history.pushState({page: "another"}, "another page",);
-//     //location.href = 'http://127.0.0.1:5500/'+Math.random(12,90);
-// },2000)

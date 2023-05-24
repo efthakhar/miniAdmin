e3ffee64aae22ab27eeddc20013878e8dca97e2c
Efthakhar <efthakharbin@gmail.com>
2023-05-25 02:40:38 +0600

    Fix sidebar sublinks initial active issue

--- js/dashboard-core.js
+++ js/dashboard-core.js
@@ -1,16 +1,34 @@
-// sidebar links sublinks toggle functionality
-let links = document.querySelectorAll('.admin-sidebar__link_item');
+const currentUrl = window.location.href;
+
+// sidebar sublinks toggle functionality and active class to links
+let links = document.querySelectorAll('.sidebar__link,.sidebar__sublink');
 
 for (let i = 0; i < links.length; i++) {
 
-    links[i].addEventListener('click', (e) => {
+    if (links[i].classList.contains('sidebar__link')) {
 
-        e.target.closest('.admin-sidebar__link_item')
-            .querySelector('.sidebar__link_sublinks')
-            .classList.toggle('show_sublinks')
-    })
+        links[i].addEventListener('click', (e) => {
+
+            e.target.closest('.admin-sidebar__link_item')
+                .querySelector('.sidebar__link_sublinks')
+                .classList.toggle('show_sublinks')
+        })
+
+    }
+
+    if (links[i].href === currentUrl) {
+
+        links[i].classList.add('active');
+
+        links[i].closest('.admin-sidebar__link_item')
+            .querySelector('.sidebar__link')
+            .classList.add('active');
+
+        let sublinks = links[i].closest('.admin-sidebar__link_item').querySelector('.sidebar__link_sublinks')
+        sublinks!=null? sublinks.classList.add('show_sublinks'):'';
+    }
+
 }
-
 
 // sidebar hide or show toggle function
 let toggle_sidebar_icon = document.querySelector('.menu-icon');
